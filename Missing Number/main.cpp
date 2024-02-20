020e3cc98341db0244a6a7b5659144854160375d
Kartavya Bang <bangkartavya@gmail.com>
2024-02-20 07:43:01 +0530

    02/20/2024

---
+++ Missing Number/main.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        int tSum = (nums.size()*(nums.size()+1))/2;
+
+        return tSum-sum;
+    }
+};
