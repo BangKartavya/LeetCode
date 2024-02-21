47718ef7d553ed966a881addf25bc9aeb56e3b77
Kartavya Bang <bangkartavya@gmail.com>
2024-02-21 07:58:51 +0530

    02/21/2024

---
+++ Bitwise AND of Numbers Range/main.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        
+        if(right>left) {
+            return rangeBitwiseAnd(left,right&(right-1));
+        }
+        return right;
+    }
+};
