f5ec9aff59c4304a445cc67c6bb5aee87f305f67
Kartavya Bang <bangkartavya@gmail.com>
2024-05-29 11:27:27 +0530

    05/29/2024

---
+++ Number of Steps to Reduce a Number in Binary Representation to One/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public
+    int numSteps(string s) {
+        int n = s.size();
+        int ans = n-1;
+        int carry = 0;
+
+        for(int i =n-1;i0;i--) {
+            if(s[i]-'0'+carry==1) {
+                ans++;
+                carry=1;
+            }
+        }
+        return ans+carry;
+    }
+};
