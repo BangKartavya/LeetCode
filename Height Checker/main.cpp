f7103dfed0dcebb5f6ca814e9f16ebb2db2f0181
Kartavya Bang <bangkartavya@gmail.com>
2024-06-10 09:49:37 +0530

    06/10/2024

---
+++ Height Checker/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> expected(heights.begin(),heights.end());
+
+        sort(begin(expected),end(expected));
+
+        int ans = 0;
+
+        for(int i =0;i<heights.size();i++) if(expected[i] != heights[i]) ans++;
+
+        return ans;
+
+    }
+};
