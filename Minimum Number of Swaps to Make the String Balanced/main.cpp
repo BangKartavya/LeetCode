f7a7e4f44ac585dceab18b1c3c0c8e19a9ff01f1
Kartavya Bang <bangkartavya@gmail.com>
2024-10-08 01:22:15 +0530

    10/08/2024

---
+++ Minimum Number of Swaps to Make the String Balanced/main.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int p = 0;
+
+        for(char c: s) {
+            p += (c=='[') - (c== ']');
+            if(p<0) p = 0;
+        }
+
+        return (p+1)/2;
+    }
+};
